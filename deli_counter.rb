fe0c50ac9554edaf026489697091fbe92f81ad4e
Katie <katiefaybehrmann@gmail.com>
2023-02-19 09:19:50 -0700

    commit tree b29c7226c37c7bf1269262e31fbec8e4bb5ab1ec

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+require 'pry'
+
+katz_deli = []
+
+def line(arr)
+    if arr.length == 0
+        puts "The line is currently empty."
+    else 
+        line_arr = arr.map.with_index(1) { |name, index| "#{index}. #{name}" }
+        printed_line = "The line is currently: #{line_arr.join(" ")}"
+        puts printed_line
+    end
+end
+
+def take_a_number(arr, str)
+    arr << str
+    puts "Welcome, #{str}. You are number #{arr.index(str) + 1} in line."
+end
+
+def now_serving(arr)
+    if arr.length == 0
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{arr[0]}."
+        arr.shift
+    end
+end
